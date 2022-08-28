0c99ecc7018354b6da211967d54bc007f6b760c1
inoccard <inocenciocardoso19@gmail.com>
2022-08-28 17:46:18 -0300

    Assigned null in the Port property

--- MessageQueueBasic.MessageBroker/HostConfiguration.cs
+++ MessageQueueBasic.MessageBroker/HostConfiguration.cs
@@ -3,7 +3,7 @@
 public class HostConfiguration
 {
     public string Host { get; set; }
-    public ushort Port { get; set; }
+    public ushort? Port { get; set; }
     public string VirtualHost { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
